3266ef8c2f7ede13aab0d80fbf27dcc1e5869ff9
NobleWolf42 <greenboyb1@gmail.com>
2019-07-01 19:05:43 -0400

    Almost there

--- chat/chat.js
+++ chat/chat.js
@@ -2,14 +2,17 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var connectedUsers = {};
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
+    connectedUsers['username'] = socket;
+    socket.emit("isConnected", { isConnected: true });
     socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
+        io.emit('chat message', 'username' + '<b>' + msg + '</b>');
     });
 });
 
