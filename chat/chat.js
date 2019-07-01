e5ba0db525fa5ea416b9b6510510791ae6c6f842
NobleWolf42 <greenboyb1@gmail.com>
2019-07-01 18:31:54 -0400

    Tyr 6

--- chat/chat.js
+++ chat/chat.js
@@ -3,29 +3,26 @@ var http = require('http').Server(app);
 var io = require("socket.io")(http);
 var port = process.env.PORT || 3000;
 
-
-var socket = io;
-
-socket.set("log level", 1);
+io.set("log level", 1);
 var people = {};
 
-socket.on("connection", function (client) {
+io.on("connection", function (client) {
 
     client.on("join", function (name) {
         people[client.id] = name;
         client.emit("update", "You have connected to the server.");
-        socket.sockets.emit("update", name + " has joined the server.")
-        socket.sockets.emit("update-people", people);
+        io.sockets.emit("update", name + " has joined the server.")
+        io.sockets.emit("update-people", people);
     });
 
     client.on("send", function (msg) {
-        socket.sockets.emit("chat", people[client.id], msg);
+        io.sockets.emit("chat", people[client.id], msg);
     });
 
     client.on("disconnect", function () {
-        socket.sockets.emit("update", people[client.id] + " has left the server.");
+        io.sockets.emit("update", people[client.id] + " has left the server.");
         delete people[client.id];
-        socket.sockets.emit("update-people", people);
+        io.sockets.emit("update-people", people);
     });
     
 });
