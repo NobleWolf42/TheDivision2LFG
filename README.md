8867677464f67bd01a922152c887f78556bba474
NobleWolf42 <greenboyb1@gmail.com>
2019-07-04 20:38:50 -0400

    EOD Commit

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 A LFG Service for The Division 2
 
 To Do List:
-1. Make Chat function use usernames. (Done)
+1. (Done) Make Chat function use usernames.
 2. Make Login Function.
 3. Change chat to use username from login.
-4. Intigrate pulling current stats from https://github.com/Tabwire/TheDivisionTab-API
-5. Find a way to verify account ownership.
-6. Create Sign-Up page
+4. Intigrate pulling current stats from https://github.com/Tabwire/TheDivisionTab-API via pid (Ubisoft's Player ID)
+5. Find a way to verify account ownership. (Possibly? https://github.com/r6db/r6db/wiki/Ubi-Api)
+6. Create Sign-Up page (will pull pid on account creation) (verification on account setup as well)
+    a. https://stackoverflow.com/questions/4295782/how-to-process-post-data-in-node-js
+    b.https://www.w3schools.com/nodejs/nodejs_mysql_insert.asp
--- signup.js
+++ signup.js
@@ -1,4 +1,22 @@
 var mysql = require('mysql');
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+var nickname;
+var emailaddr;
+var uppid;
+var plainpass;
+
+app.use(express.urlencoded());
+app.post('/', function (request, response) {
+    nickname = request.body.user.name;
+    emailaddr = request.body.user.email;
+    uppid = request.body.user.pi;
+    plainpass = request.body.user.pass;
+});
+
+var passhash = bcrypt.hash(plainpass, saltRounds, function (err, hash) { });
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -9,4 +27,10 @@ var con = mysql.createConnection({
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
+    var sql = "INSERT INTO users (username, e-mail, pid, password) VALUES ?";
+    var values = [nickname, emailaddr, uppid, passhash];
+    con.query(sql, [values], function (err, result) {
+        if (err) throw err;
+        console.log("Number of records inserted: " + result.affectedRows);
+    });
 });
